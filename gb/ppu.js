d1edff7061d75dbbdf268d2b3a6992076cf642a6
griffi-gh <prasol258@gmail.com>
2021-09-05 21:14:44 +0300

    Update ppu.js

--- gb/ppu.js
+++ gb/ppu.js
@@ -1,4 +1,5 @@
 import PixelCanvas from './pxcanvas.js';
+import {u8, u16} from './common.js';
 
 export default class PPU {
   constructor(gb, id) {
@@ -6,5 +7,11 @@ export default class PPU {
     this.screenSize = [160, 144];
     this.vram = new Array(0x2000).fill(0);
     this.canvas = new PixelCanvas(id, this.screenSize);
+  }
+  writeVRAM(addr, val) {
+    this.vram[addr] = val;
+  }
+  readVRAM(addr, val) {
+    return this.vram[addr];
   }
 }
