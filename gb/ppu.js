07b6fd77a574d3ebf561923e8d5b2c8ccf8fb8a8
griffi-gh <prasol258@gmail.com>
2021-09-10 00:55:09 +0300

    e

--- gb/ppu.js
+++ gb/ppu.js
@@ -6,11 +6,18 @@ export default class PPU {
     this.screenSize = [160, 144];
     this.vram = new Array(0x2000).fill(0);
     this.canvas = new PixelCanvas(id, this.screenSize);
+    this.scx = 0;
+    this.scy = 0;
+    this.lcdOn = true;
+    this.clk = 0;
   }
   writeVRAM(addr, val) {
     this.vram[addr] = val;
   }
   readVRAM(addr, val) {
     return this.vram[addr];
+  }
+  tick() {
+
   }
 }
