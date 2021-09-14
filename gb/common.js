8aa4de894aba7e077ac33444e322cc53c4c9d101
griffi-gh <prasol258@gmail.com>
2021-09-15 02:07:00 +0300

    upd

--- gb/common.js
+++ gb/common.js
@@ -1,10 +1,3 @@
-/*export function u8(a) {
-  return (a & 0xFF);
-}
-export function u16(a){
-  return (a & 0xFFFF);
-}*/
-
 export function i8(v) {
   return v & 0x80 ? v-256 : v;
 }
@@ -13,4 +6,17 @@ export function toHex(num, size = 8) {
   if(typeof num !== 'number') { return 'INVALID'; }
   const c = (size / 4);
   return ('0'.repeat(c-1) + num.toString(16)).slice(-c).toUpperCase();
+}
+
+function downloadString(text = '', fileName = 'download.txt', fileType = 'text/plain') {
+  const blob = new Blob([text], { type: fileType });
+  const a = document.createElement('a');
+  a.download = fileName;
+  a.href = URL.createObjectURL(blob);
+  a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
 }
--- gb/gb.js
+++ gb/gb.js
@@ -1,23 +1,10 @@
+import { toHex, downloadString } from './common.js';
+
 import CPU from './cpu.js';
 import MMU from './mmu.js';
 import PPU from './ppu.js';
 
-import { toHex } from './common.js';
-
 const CYCLES_PER_FRAME = 70224;
-
-function downloadString(text = '', fileName = 'download.txt', fileType = 'text/plain') {
-  const blob = new Blob([text], { type: fileType });
-  const a = document.createElement('a');
-  a.download = fileName;
-  a.href = URL.createObjectURL(blob);
-  a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
-  a.style.display = "none";
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
-  setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
-}
 
 export class Gameboy {
   constructor(id) {
