036c86401a231b5d41917ded649707d751842aea
griffi-gh <prasol258@gmail.com>
2021-09-19 21:01:16 +0300

    Update cartridge.js

--- gb/cartridge.js
+++ gb/cartridge.js
@@ -1,4 +1,4 @@
-export class Cartridge {
+export class CartridgeNone {
 	constructor() {
 		this.data = new Uint8Array(0x8000).fill(0);
 	}
@@ -9,32 +9,62 @@ export class Cartridge {
 	      this.rom[i] = (d[i] | 0);
 	    }
 	}
-	writeROM(a) {}
-	readROM(a) { return data[a]; }
-	writeERAM(a, v) {}
-	readERAM(a, v) { return 0; }
-}
-
-export class MBC1 extends Cartridge {
-	constructor() {
-		super();
-		this.romBank = 1;
-		this.romBankAmount = 1;
-	}
-	writeROM(a) {
-		if((a >= 0x2000) && (a <= 0x3FFF)) {
-
-		}
-	}
-	readROM(a) {
-		if(a >= 0x4000) {
-
-		} else {
+	read(a) {
+		if(a <= 0x7FFF) {
 			return this.data[a];
 		}
+		return 0;
+	}
+	write(a, v) {}
+}
+
+export class CartridgeMBC1 extends Cartridge {
+	constructor() {
+		super();
+		
+		this.ramBank = 0;
+		this.ramEnable = false;
+
+		this.romBank = 1;
+		this.romBankAmount = Math.ceil(this.data / 0x4000);
+	}
+	write(a, v) {
+		if(a >= 0x2000) && (a <= 0x3FFF) {
+			v &= 0x1F;
+			v = ((v == 0) ? 1 : v);
+			this.romBank = v;
+			return;
+		}
+	}
+	read(a) {
+		if(a <= 0x7FFF) {
+			if(a >= 0x4000) {
+				const ra = (this.romBank * 0x4000) + (a % 0x4000);
+				return this.data[ra]
+			} else {
+				return this.data[a];
+			}
+		}
+		return 0;
 	}
 }
 
-export class MBC1Ram extends MBC1 {
+export class CartridgeMBC1Ram extends CartridgeMBC1 {
+	constructor() {
+		super();
+	}
+	read(a) {
+		return super.read(a);
+	}
+}
 
+export function Cartridge(i) {
+	switch (i) {
+		case 0x00:
+			return CartridgeNone;
+		case 0x01:
+			return CartridgeMBC1;
+		default:
+			throw new Error("Invalid MBC type");
+	}
 }
