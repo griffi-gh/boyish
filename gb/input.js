f232e62aabf745bdb1f4499534f88d263d5b816e
griffi-gh <prasol258@gmail.com>
2021-10-06 02:35:43 +0300

    Update input.js

--- gb/input.js
+++ gb/input.js
@@ -1,12 +1,23 @@
+const kbMap = {
+  ArrowRight: 'RIGHT',
+  ArrowLeft:  'LEFT',
+  ArrowUp:    'UP',
+  ArrowDown:  'DOWN',
+  KeyZ:       'A',
+  KeyX:       'B',
+  Space:      'SELECT',
+  Enter:      'START',
+}
+
 const keyMap = {
-  ArrowRight: (1 << 0),
-  ArrowLeft:  (1 << 1),
-  ArrowUp:    (1 << 2),
-  ArrowDown:  (1 << 3),
-  KeyZ:  (1 << 4),
-  KeyX:  (1 << 5),
-  Space: (1 << 6),
-  Enter: (1 << 7),
+  RIGHT:  (1 << 0),
+  LEFT:   (1 << 1),
+  UP:     (1 << 2),
+  DOWN:   (1 << 3),
+  A:      (1 << 4),
+  B:      (1 << 5),
+  SELECT: (1 << 6),
+  START:  (1 << 7),
 }
 
 export default class Input {
@@ -27,21 +38,30 @@ export default class Input {
   set joyp(v) {
     this.select = v & 0x30;
   }
-  inputHandler(ev) {
+  inputHandler(isDown, key) {
+    console.log(isDown, key);
     if(!this.gb.paused) {
-      const code = ev.code;
-      if(code in keyMap) {
-        const keyMask = keyMap[code];
-        const isCurrent = ((keyMask >= 0x10 && this.select == 0x10) || (keyMask < 0x10 && this.select == 0x20));
-        if(event.type === 'keydown') {
-          this.keyState &= (~keyMask) & 0xFF;
-          if(isCurrent) {
-            //Raise JOYP interrupt
-            this.gb.cpu.irq.if |= 0x10;
-          }
-        } else {
-          this.keyState |= keyMask;
+      const keyMask = keyMap[key];
+      const isCurrent = ((keyMask >= 0x10 && this.select == 0x10) || (keyMask < 0x10 && this.select == 0x20));
+      if(isDown) {
+        this.keyState &= (~keyMask) & 0xFF;
+        if(isCurrent) {
+          this.gb.cpu.irq.if |= 0x10; //Raise JOYP interrupt
         }
+      } else {
+        this.keyState |= keyMask;
+      }
+    }
+  }
+  inputEventHandler(ev) {
+    if(!this.gb.paused) {
+      if(ev.repeat) return;
+      const code = ev.code;
+      if(code in kbMap) {
+        this.inputHandler(
+          event.type === 'keydown',
+          kbMap[code]
+        );
         ev.preventDefault();
         ev.stopPropagation();
       }
@@ -49,7 +69,7 @@ export default class Input {
   }
   enable() {
     if(!this.enabled) {
-      this._callback = this.inputHandler.bind(this);
+      this._callback = this.inputEventHandler.bind(this);
       document.addEventListener('keydown', this._callback);
       document.addEventListener('keyup', this._callback);
       this.enabled = true;
